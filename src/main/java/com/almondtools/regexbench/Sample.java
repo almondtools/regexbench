d4fbd138f07345cd29ffb3fdfa84f6fd64d76e74
Stefan Mandel <mandelst@gmail.com>
2015-11-02 07:07:50 +0100

    new sample

--- src/main/java/com/almondtools/regexbench/Sample.java
+++ src/main/java/com/almondtools/regexbench/Sample.java
@@ -18,7 +18,7 @@ import org.openjdk.jmh.annotations.TearDown;
 @State(Scope.Benchmark)
 public class Sample {
 
-	@Param({"test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9"})
+	@Param({"test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9","test10"})
 	private String name;
 	private String sample;
 	private String pattern;
